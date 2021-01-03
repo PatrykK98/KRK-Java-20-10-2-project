660398f3a5c08c50fb6c635d96697cce80f7ab1b
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2021-01-04 00:28:27 +0100

    Update Player.java

--- src/main/java/pl/itacademy/tictac/Player.java
+++ src/main/java/pl/itacademy/tictac/Player.java
@@ -1,7 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Player {
 public String name;
 private String password;
 protected Statistic statistic;
 protected char X,O;
+
+//private Map<String,String> playersList = new HashMap<>();
+Scanner input = new Scanner(System.in);
+
+public Player(String name) {
+	this.name = name;
+}
+public Player(String name, String password) {
+	this.name=name;
+	this.password=password;
+}
+public void register() {	
+}
+public void login() {
+}
+public Statistic viewOwnStatic() {
+	return statistic;
+}
+public void createGame() {
+}
+public void leaveGame() {
+}
+public void move() {
+}
 }
